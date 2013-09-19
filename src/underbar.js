c21ee555fa618a37e2b90489e0603adb9c73f33b
Christian Monaghan <christian.monaghan@gmail.com>
2013-09-18 18:12:31 -0700

    Added '_.first' functionality

--- src/underbar.js
+++ src/underbar.js
@@ -16,6 +16,13 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if(n == undefined) {
+      return array[0];
+    } else if (n > array.length) {
+      return array.splice(0,array.length);
+    } else {
+      return array.splice(0,n);
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
