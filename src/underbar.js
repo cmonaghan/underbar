d698a589414058e711fe7e2678acf225b4c2dd60
Christian Monaghan <christian.monaghan@gmail.com>
2013-10-10 09:49:20 -0700

    Added _.contains functionality

--- src/underbar.js
+++ src/underbar.js
@@ -169,12 +169,19 @@ var _ = { };
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    return _.reduce(collection, function(wasFound, item) {
+    /*return _.reduce(collection, function(wasFound, item) {
       if(wasFound) {
         return true;
       }
       return item === target;
-    }, false);
+    }, false);*/
+    var result = false
+    for (var key in collection) {
+      if (collection[key] === target){
+        result = true;
+      }
+    };
+    return result;
   };
 
 
