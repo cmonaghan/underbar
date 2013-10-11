c6c92884bbfc5fdd40508b391ef697f1d4cbc829
Christian Monaghan <christian.monaghan@gmail.com>
2013-10-11 15:56:43 -0700

    Added _.delay code, non-working

--- src/underbar.js
+++ src/underbar.js
@@ -306,7 +306,8 @@ var _ = { };
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
-    return setTimeout(func, wait);
+    //var args = Array.prototype.slice.call(arguments, 2);
+    return setTimeout(func, wait, arguments);
   };
 
 
